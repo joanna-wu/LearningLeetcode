2cd2c62e2f5cad72e616a42bc7830756d42bdeb7
Na Wu <joanna.uta@gmail.com>
2016-01-04 12:01:13 -0600

    1

--- Q27.java
+++ Q27.java
@@ -1,13 +1,13 @@
 public class solution{
 	public int removeElement(int[] nums, int val){
 		int N = nums.length;
-		int lens = N;
 		for(int i=0; i<N; i++){
 			if(nums[i] == val){
-				lens--;
-				nums[i]=Integer.MIN_VALUE;
+				nums[i]=nums[N-1];
+				N--;
+				i--;
 			}
 		}
-		return lens;
+		return N;
 	}
 }
